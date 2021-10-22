2427a432d7cfd1a0e8d1332f76ef2eb392ef8f0f
aajusah98 <computer9860@gmail.com>
2021-10-23 00:51:41 +0530

    httpclient crossorgin

--- src/main/java/com/example/donatekaro/controller/UserController.java
+++ src/main/java/com/example/donatekaro/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin(origins = "https://donatekarobackend.herokuapp.com")
 @RestController
 public class UserController {
 
